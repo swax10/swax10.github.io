7edbf86ba82649a037b4b332e54d7a145be663d4
SWAX10 <110764543+swax10@users.noreply.github.com>
2022-10-26 05:32:38 -0700

    Update a.sh

--- a.sh
+++ a.sh
@@ -534,119 +534,87 @@ EOF
 						cd ffill
 						touch ffill.c
 						cat > ffill.c << EOF
+#include <iostream>
 #include <math.h>
+#include <time.h>
 #include <GL/glut.h>
 
-struct Point {
-	GLint x;
-	GLint y;
-};
+using namespace std;
 
-struct Color {
-	GLfloat r;
-	GLfloat g;
-	GLfloat b;
-};
-
-void draw_dda(Point p1, Point p2) {
-	GLfloat dx = p2.x - p1.x;
-	GLfloat dy = p2.y - p1.y;
-
-	GLfloat x1 = p1.x;
-	GLfloat y1 = p1.y;
-
-	GLfloat step = 0;
-
-	if(abs(dx) > abs(dy)) {
-		step = abs(dx);
-	} else {
-		step = abs(dy);
-	}
-
-	GLfloat xInc = dx/step;
-	GLfloat yInc = dy/step;
-
-	for(float i = 1; i <= step; i++) {
-		glVertex2i(x1, y1);
-		x1 += xInc;
-		y1 += yInc;
-	}
+void delay(float ms) {
+    clock_t goal = ms + clock();
+    while (goal > clock());
 }
 
 void init() {
-	glClearColor(1.0, 1.0, 1.0, 0.0);
-	glColor3f(0.0, 0.0, 0.0);
-	glPointSize(1.0);
-	glMatrixMode(GL_PROJECTION);
-	glLoadIdentity();
-	gluOrtho2D(0, 640, 0, 480);
+    glClearColor(0.0, 0.0, 0.0, 0.0);
+    glMatrixMode(GL_PROJECTION);
+    gluOrtho2D(0, 640, 0, 480);
 }
 
-Color getPixelColor(GLint x, GLint y) {
-	Color color;
-	glReadPixels(x, y, 1, 1, GL_RGB, GL_FLOAT, &color);
-	return color;
+void flood_it(int x, int y, float *fillColor, float *ic) {
+    float color[3];
+    glReadPixels(x, y, 1.0, 1.0, GL_RGB, GL_FLOAT, color);
+    if (color[0] == ic[0] && color[1] == ic[1] && color[2] == ic[2]) {
+        glColor3f(fillColor[0], fillColor[1], fillColor[2]);
+        glBegin(GL_POINTS);
+        glVertex2i(x, y);
+        glEnd();
+        glFlush();
+        flood_it(x - 2, y, fillColor, ic);
+        flood_it(x + 1, y, fillColor, ic);
+        flood_it(x, y + 1, fillColor, ic);
+        flood_it(x, y - 2, fillColor, ic);
+    }
 }
 
-void setPixelColor(GLint x, GLint y, Color color) {
-	glColor3f(color.r, color.g, color.b);
-	glBegin(GL_POINTS);
-		glVertex2i(x, y);
-	glEnd();
-	glFlush();
+void mouse(int btn, int state, int x, int y) {
+    y = 480 - y;
+    if (btn == GLUT_LEFT_BUTTON) {
+        if (state == GLUT_DOWN) {
+            float intCol[] = {1, 0, 0};
+            float color[] = {0, 0, 1};
+            glReadPixels(x, y, 1.0, 1.0, GL_RGB, GL_FLOAT, intCol);
+            flood_it(x, y, color, intCol);
+        }
+    }
 }
 
-void floodFill(GLint x, GLint y, Color oldColor, Color newColor) {
-	Color color;
-	color = getPixelColor(x, y);
-
-	if(color.r == oldColor.r && color.g == oldColor.g && color.b == oldColor.b)
-	{
-		setPixelColor(x, y, newColor);
-		floodFill(x+1, y, oldColor, newColor);
-		floodFill(x, y+1, oldColor, newColor);
-		floodFill(x-1, y, oldColor, newColor);
-		floodFill(x, y-1, oldColor, newColor);
-	}
-	return;
+void world() {
+    glPointSize(2);
+    glClear(GL_COLOR_BUFFER_BIT);
+    glColor3f(1, 0, 0);
+    glBegin(GL_POLYGON);
+    glVertex2i(15, 10);
+    glVertex2i(155, 200);
+    glVertex2i(305, 10);
+    glEnd();
+    glColor3f(0, 1, 0);
+    glBegin(GL_POLYGON);
+    glVertex2i(300, 398);
+    glVertex2i(150, 198);
+    glVertex2i(450, 198);
+    glEnd();
+    glColor3f(1, 0.7, 0);
+    glBegin(GL_POLYGON);
+    glVertex2i(300, 10);
+    glVertex2i(600, 10);
+    glVertex2i(450, 200);
+    glEnd();
+    glFlush();
 }
 
-void onMouseClick(int button, int state, int x, int y)
-{
-	Color newColor = {1.0f, 0.0f, 0.0f};
-	Color oldColor = {1.0f, 1.0f, 1.0f};
-
-	floodFill(101, 199, oldColor, newColor);
-}
-
-void display(void) {
-	Point p1 = {100, 100}, // bottom-right
-		p2 = {200, 100}, // bottom-left
-		p3 = {200, 200}, // top-right
-		p4 = {100, 200}; // top-left
-	
-	glClear(GL_COLOR_BUFFER_BIT);
-	glBegin(GL_POINTS);
-		draw_dda(p1, p2);
-		draw_dda(p2, p3);
-		draw_dda(p3, p4);
-		draw_dda(p4, p1);
-	glEnd();
-	glFlush();
-}
-
-int main(int argc, char** argv)
-{
-	glutInit(&argc, argv);
-	glutInitDisplayMode(GLUT_SINGLE|GLUT_RGB);
-	glutInitWindowSize(640, 480);
-	glutInitWindowPosition(200, 200);
-	glutCreateWindow("Open GL");
-	init();
-	glutDisplayFunc(display);
-	glutMouseFunc(onMouseClick);
-	glutMainLoop();
-	return 0;
+int main(int argc, char **argv) {
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
+    glutInitWindowSize(640, 480);
+    glutInitWindowPosition(200, 200);
+    glutCreateWindow("Flood Fill");
+    glutDisplayFunc(world);
+    glutMouseFunc(mouse);
+    init();
+    glutMainLoop();
+    return 0;
 }
 EOF
 		g++ ffill.c -lGL -lGLU -lglut -lm
