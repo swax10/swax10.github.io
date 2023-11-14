14b39adfa2f76f97ee4293efac2116f7e06c9bf3
SWAX10 <110764543+swax10@users.noreply.github.com>
2023-11-14 18:35:11 +0530

    Kubeflow

--- ngrok.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-wget https://bin.equinox.io/c/bNyj1mQVY4c/ngrok-v3-stable-linux-amd64.tgz
-tar -xvzf ngrok-v3-stable-linux-amd64.tgz
-sudo mv ngrok /usr/local/cd/bin/
-chmod +x /usr/local/bin/ngrok
-echo "THE END"
