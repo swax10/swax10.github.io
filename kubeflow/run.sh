b71df73f5832b1ae40b4dcd8d9fcd03fbba3f335
SWAX10 <110764543+swax10@users.noreply.github.com>
2023-11-14 18:51:03 +0530

    Delete kubeflow/run.sh

--- kubeflow/run.sh
+++
@@ -1,7 +0,0 @@
-export PIPELINE_VERSION=2.0.3
-kubectl apply -k "github.com/kubeflow/pipelines/manifests/kustomize/cluster-scoped-resources?ref=$PIPELINE_VERSION"
-kubectl wait --for condition=established --timeout=60s crd/applications.app.k8s.io
-kubectl apply -k "github.com/kubeflow/pipelines/manifests/kustomize/env/platform-agnostic-pns?ref=$PIPELINE_VERSION"
-
-ngrok config add-authtoken $1
-ngrok http 80
