f9f3a5baf06b3a5e71e5f3521820f93210cf17ef
SWAX10 <110764543+swax10@users.noreply.github.com>
2023-11-14 17:46:37 +0530

    ngrok

--- ngrok.sh
+++ ngrok.sh
@@ -1,12 +1,6 @@
 #!/bin/bash
-mkdir /var/log/ngrok && chmod -R +x /var/log
-mkdir /root/.ngrok2 && chmod -R +x /root/.ngrok2
-mkdir /temp_docker && chmod -R +x /temp_docker && cd /temp_docker
-wget https://raw.githubusercontent.com/akhomy/alpine-ngrok/master/ngrok.yml
-mv ngrok.yml /root/.ngrok2/
-wget https://raw.githubusercontent.com/akhomy/alpine-ngrok/master/ngrok-stable-linux-386.zip
-mv ngrok-stable-linux-386.zip /temp_docker/ngrok-stable-linux-386.zip
-cd /temp_docker && unzip /temp_docker/ngrok-stable-linux-386.zip
-cp /temp_docker/ngrok /usr/local/bin/
+wget https://bin.equinox.io/c/bNyj1mQVY4c/ngrok-v3-stable-linux-amd64.tgz
+tar -xvzf ngrok-v3-stable-linux-amd64.tgz
+sudo mv ngrok /usr/bin/
 chmod +x /usr/local/bin/ngrok
-rm -rf /temp_docker
+echo "THE END"
