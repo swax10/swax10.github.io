17e453f49ec269f357a56b05abd650383ced2986
SWAX10 <110764543+swax10@users.noreply.github.com>
2023-11-14 18:47:39 +0530

    kubeflow

--- kubeflow/setup.sh
+++ kubeflow/setup.sh
@@ -4,11 +4,3 @@ tar -xvzf ngrok-v3-stable-linux-amd64.tgz
 sudo cp ngrok /usr/local/cd/bin/
 chmod +x /usr/local/bin/ngrok
 echo "Ngrok Successfully Installed"
-
-export PIPELINE_VERSION=2.0.3
-kubectl apply -k "github.com/kubeflow/pipelines/manifests/kustomize/cluster-scoped-resources?ref=$PIPELINE_VERSION"
-kubectl wait --for condition=established --timeout=60s crd/applications.app.k8s.io
-kubectl apply -k "github.com/kubeflow/pipelines/manifests/kustomize/env/platform-agnostic-pns?ref=$PIPELINE_VERSION"
-
-ngrok config add-authtoken $1
-ngrok http 80
