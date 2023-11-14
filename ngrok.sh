50519b08b5705191e61f377b724ad2d15223df82
SWAX10 <110764543+swax10@users.noreply.github.com>
2023-11-14 17:54:33 +0530

    ngrok

--- ngrok.sh
+++ ngrok.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 wget https://bin.equinox.io/c/bNyj1mQVY4c/ngrok-v3-stable-linux-amd64.tgz
 tar -xvzf ngrok-v3-stable-linux-amd64.tgz
-sudo cp ngrok /usr/local/cd/bin/
+sudo mv ngrok /usr/local/cd/bin/
 chmod +x /usr/local/bin/ngrok
 echo "THE END"
