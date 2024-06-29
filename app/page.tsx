c7aeb3cd565f4204fd8f7b611d3c8c3a1599271b
swax10 <shreyasw496@gmail.com>
2024-06-29 16:56:45 +0530

    chore: Update webpage metadata and add redirects

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,129 @@
-import Image from "next/image";
+import CV from "./components/CV";
+import Github from "./components/Github";
+import GoogleScholar from "./components/GoogleScholar";
+import LinkedIn from "./components/LinkedIn";
+import X from "./components/X";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <div className="flex flex-col sm:flex-row p-5 sm:p-20">
+      <header className="flex flex-col items-center sm:items-start">
+        <h1 className="text-2xl sm:text-4xl text-nowrap font-bold pb-5">
+          Shreyas Waghmode
+        </h1>
+        <div>
+          <img
+            className="w-48 h-48 sm:w-72 sm:h-72 rounded-full"
+            src="https://avatars.githubusercontent.com/u/110764543?v=4"
+            alt=""
+          />
+          <div className="flex flex-row justify-center pt-5">
+            <CV />
+            <Github />
+            <GoogleScholar />
+            <X />
+            <LinkedIn />
+          </div>
         </div>
-      </div>
+      </header>
+      <section className="pl-5 sm:pl-10">
+        <h1 className="font-semibold text-gray-800 text-xl sm:text-2xl">👋 Hi,</h1>
+        <p>
+          I’m Shreyas Waghmode, an undergraduate student pursuing Artificial
+          Intelligence and Data Science at
+          <a className="text-red-400 font-bold" href="https://nhitm.ac.in/">
+            {" "}
+            NHITM
+          </a>
+          , Thane. I have the privilege of being advised by
+          <a
+            href="https://www.linkedin.com/in/dr-megha-v-gupta-78650b169/"
+            className="text-red-400 font-bold"
+          >
+            {" "}
+            Dr. Megha V Gupta
+          </a>{" "}
+          &
+          <a
+            href="https://www.linkedin.com/in/suhas-waghmare-975277131/"
+            className="text-red-400 font-bold"
+          >
+            {" "}
+            Asst Prof.Suhas Waghmare
+          </a>
+          . My research interests revolve around
+          <strong className="text-lg font-semibold text-gray-800"> Multimodal AI</strong>.
+        </p>
+        <br />
+        <h2 className="text-lg font-semibold text-gray-800 my-4">
+          Here’s a deeper dive into my interests and experiences:
+        </h2>
+        <p>
+          <h2 className="text-lg font-semibold text-gray-800">
+            Research Focus 🔭:
+          </h2>{" "}
+          My passion lies in Multimodal AI, where I explore the intersection of
+          computer vision, natural language processing, and audio analysis. I'm
+          fascinated by how different modalities can enhance each other, leading
+          to more robust and context-aware AI systems.
+          
+        </p>
+        <p>
+          <h2 className="text-lg font-semibold text-gray-800 pt-5">
+            Projects 🚀:
+          </h2>{" "}
+          Besides my coursework, I've worked on exciting projects. One of them
+          is the MultimodalDataLoader, a tool I developed for efficient data
+          loading in both JAX and PyTorch. It's been a rewarding experience, and
+          I'm always eager to contribute to open-source projects.
+        </p>
+        <p>
+          <h2 className="text-lg font-semibold text-gray-800 pt-5">
+            Languages and Tools 💻 :
+          </h2>{" "}
+          I'm proficient in Python (especially with libraries like PyTorch and
+          JAX), but I also have a soft spot for Rust and Haiku. Exploring
+          different programming paradigms keeps me curious and motivated.
+        </p>
+        <p>
+          <h2 className="text-lg font-semibold text-gray-800 pt-5">
+            Gaming Enthusiast 🎮:
+          </h2>{" "}
+          When I'm not immersed in code or research papers, you'll find me
+          battling virtual dragons or exploring distant galaxies in video games.
+          Gaming fuels my creativity and problem-solving skills.
+        </p>
 
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+        <h3 className="text-lg font-semibold text-gray-800 pt-10"> Research Highlights </h3>
+        <table className="table-auto">
+          <tbody>
+            <tr>
+              {" "}
+              {/* Add this line */}
+              <td>
+                <br />
+                <div>
+                  <b>
+                    <a
+                      href="https://drive.google.com/file/d/1HCmImT9bDwDw_5ZfOt5hkisY6O8mMSGC/view?usp=sharing"
+                      className="text-red-400 font-bold"
+                    >
+                      Introduction to Multimodal Deep Learning 🌟
+                    </a>
+                  </b>
+                </div>
+                <div>
+                  Introduction to Multimodal Deep Learning is a comprehensive
+                  guide to understanding the basics of multimodal deep learning.
+                  It covers the fundamentals of multimodal learning, its
+                  applications, and the challenges faced in the field.
+                </div>
+              </td>
+            </tr>{" "}
+            {/* Add this line */}
+          </tbody>
+        </table>
+      </section>
+    </div>
   );
 }
--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,19 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    async redirects() {
+        return [
+          {
+            source: '/multivet',
+            destination: 'https://colab.research.google.com/drive/1A8puLHAugA62UxLJvCquXCYgXHvMIudd?usp=sharing',
+            permanent: true,
+          },
+          {
+            source: '/meco',
+            destination: 'https://github.com/swax10/meco',
+            permanent: true,
+          }
+        ]
+      },
+};
 
 export default nextConfig;
